24a044a8cbd6d21c5f2269ad4e730b08b6e72acb
Daniel Gauert <daniel.gauert@kzvk.de>
2023-09-19 11:08:53 +0200

    Neue Tests

--- test_input_validation.py
+++ test_input_validation.py
@@ -9,6 +9,7 @@ from input_validation import is_valid_email
 ,   ("te-st@email.com")
 ,   ("te_st@email.com")
 ,   ("test1@email.com")
+
 ])
 def test_is_valid_email__gueltige_Adressen(email):
     assert is_valid_email(email) == True
@@ -18,6 +19,9 @@ def test_is_valid_email__gueltige_Adressen(email):
     ("testemail.com")   # Fehlendes @-Zeichen
 ,   ("test@email")      # Fehlende Top-Level-Domain
 ,   ("test@em@ail.com") # Mehrfaches @-Zeichen
+,   ("invalid_email")
+,   ("testemail.com")
 ])
 def test_is_valid_email__ungueltige_Adressen(email):
     assert is_valid_email(email) == False
+    
